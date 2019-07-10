67bda94001c7af349ba9c3650c2d2a6a0e84bb7f
Kael Zhang <i+github@kael.me>
2019-07-10 14:16:02 +0800

    more tests

--- test/bin-tool.test.js
+++ test/bin-tool.test.js
@@ -2,29 +2,67 @@ const test = require('ava')
 // const log = require('util').debuglog('bin-tool')
 const {run} = require('./run')
 
-test('normal: default value', async t => {
-  const content = await run('normal', ['log'])
-
-  t.is(content, JSON.stringify({
-    port: 3000,
-    verbose: false
-  }))
-})
-
-test('normal: alias command', async t => {
-  const content = await run('normal', ['lg'])
-
-  t.is(content, JSON.stringify({
-    port: 3000,
-    verbose: false
-  }))
-})
-
-test('normal: no sub command', async t => {
-  const content = await run('normal', [])
-
-  t.is(content, `bin [command]
+const CASES = [
+  [
+    'default value',
+    ['normal', ['log']],
+    {
+      port: 3000,
+      verbose: false
+    }
+  ],
+  [
+    'offset = 3, default value',
+    ['normal', ['xx', 'log'], {
+      OFFSET: '3'
+    }],
+    {
+      port: 3000,
+      verbose: false
+    }
+  ],
+  [
+    'with options',
+    [
+      'normal',
+      ['log', '--port', '7001', '--no-verbose']
+    ],
+    {
+      port: 7001,
+      verbose: false
+    }
+  ],
+  [
+    'alias command',
+    ['normal', ['lg']],
+    {
+      port: 3000,
+      verbose: false
+    }
+  ],
+  [
+    'no sub command',
+    ['normal', []],
+    `bin [command]
 
 Commands:
-  log, lg`)
+  log, lg`
+  ],
+  [
+    'version',
+    ['normal', ['--version']],
+    '1.0.0'
+  ]
+]
+
+CASES.forEach(([d, args, ex]) => {
+  test(`${args[0]}: ${d}`, async t => {
+    const content = await run(...args)
+    t.is(
+      content,
+      Object(ex) === ex
+        ? JSON.stringify(ex)
+        : ex
+    )
+  })
 })
--- test/normal/index.js
+++ test/normal/index.js
@@ -4,5 +4,11 @@ module.exports = Command => class extends Command {
 
     this.load(require('path').join(__dirname, 'command'))
     this.alias('lg', 'log')
+
+    this.version = '1.0.0'
+
+    if (process.env.OFFSET) {
+      this.offset = Number(process.env.OFFSET)
+    }
   }
 }
